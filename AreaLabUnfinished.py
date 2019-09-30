b06063d6669f848c2800b98328af94084ec953c9
Luca <lucaccorey@gmail.com>
2019-09-30 09:57:47 -0700

    Python area formulas and test

--- AreaLabUnfinished.py
+++ AreaLabUnfinished.py
@@ -1,24 +1,29 @@
 import math
 import unittest
 
+
 def circleArea(radius):
-    return 1
+    """Return the area of a circle"""
+    return math.pi * radius * radius
+
 
 def rectArea(base, height):
-    return 1
+    return base * height
+
 
 def trapArea(base1, base2, height):
-    return 1
+    return ((base1 + base2) / 2) * height
 
-def triArea(base,height):
-    return 1
+
+def triArea(base, height):
+    return (base * height) / 2
 
 
 class MyTest(unittest.TestCase):
     def testCircleArea(self):
-        self.assertEqual():
-    def testRectArea(self):
+        self.assertEqual(circleArea(5), 25 * math.pi)
+    """def testRectArea(self):
 
     def testTrapArea(self):
 
-    def testTriArea(self):
+    def testTriArea(self):"""
